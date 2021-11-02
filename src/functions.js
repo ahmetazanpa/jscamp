c1645317e9307cb8d21128c7efb42f9901717594
AHMET SELÇUK AZANPA <ahmetazanpa@hotmail.com>
2021-11-02 23:15:32 +0300

    2.gün calismalari

---
+++ src/functions.js
@@ -0,0 +1,66 @@
+function addToCart(quantity, productName = "Elma" ) {
+    console.log("Sepete Eklendi :  Ürün:" + productName, "adet : " + quantity)
+}
+
+
+//addToCart()
+addToCart(10)
+//addToCart("Karpuz")
+
+let sayHello = () => {
+    console.log("Hello World")
+}
+
+sayHello()
+
+let sayHello2 = function () {
+    console.log("Hello world 2")
+}
+
+sayHello2()
+
+
+function addToCart2(productName, quantity, unitPrice){
+
+}
+
+addToCart2("Elma", 5,10)
+addToCart2("Armut", 2,20)
+addToCart2("Limon", 3,15)
+
+
+let product1 = { productName: "Elma", unitPrice: 10, quantity: 5 }
+
+function addToCart3(product){
+    console.log("Ürün : " + product.productName)
+    console.log("Adet : " + product.quantity)
+    console.log("Fiyat : " + product.unitPrice)
+}
+
+
+addToCart3(product1)
+
+let product2 = { productName: "Elma", unitPrice: 10, quantity: 5 }
+let product3 = { productName: "Elma", unitPrice: 10, quantity: 5 }
+
+product2 = product3
+product2.productName = "KARPUZ"
+console.log(product3.productName)
+
+let sayi1 = 10;
+let sayi2 = 20;
+sayi1 = sayi2
+sayi2 = 100
+console.log(sayi1)
+
+function addToCart4(x) {
+    console.log(products)
+}
+
+let products = [
+    { productName: "Elma", unitPrice: 10, quantity: 5 },
+    { productName: "Armut", unitPrice: 10, quantity: 5 },
+    { productName: "Karpuz", unitPrice: 10, quantity: 5 }
+]
+
+addToCart4(products)
---
+++ src/homework/day2.js
@@ -0,0 +1,72 @@
+/* 2.Ders Ödevi */
+
+//Ödev 1
+function findPrime(...numbers) {
+    numbers.map((number, index) => {
+        number % index == 0 ? console.log(`${number} sayısı asal`)
+            : console.log(`${number} sayısı asal değil`)
+    })
+}
+
+findPrime(2, 5, 8, 21, 13);
+findPrime(3, 5)
+console.log("-".repeat(10))
+
+//Ödev 2
+function friendNumbers(number1, number2) {
+    let sum1 = 0, sum2 = 0;
+    for (let i = 1; i < number1; i++) {
+        if (number1 % i == 0) {
+            sum1 = sum1 + i;
+        }
+    }
+
+    for (let i = 1; i < number2; i++) {
+        if (number2 % i == 0) {
+            sum2 = sum2 + i;
+        }
+    }
+
+    if (number1 == sum2 && number2 == sum1) {
+        console.log(`${number1} ve ${number2} arkadaş sayılardır`);
+    }
+    else {
+        console.log(`${number1} ve ${number2} arkadaş sayı değillerdir`);
+    }
+}
+
+friendNumbers(124, 150)
+friendNumbers(220, 284)
+console.log("-".repeat(10))
+
+//Ödev 3
+function perfectNumbers() {
+    let sum = 0;
+    for (let i = 1; i < 1000; i++) {
+        for (let x = 1; x < i; x++) {
+            if (i % x == 0) sum = sum + x;
+        }
+        if (sum == i) console.log(`${i} sayısı mükemmel sayıdır.`)
+        sum = 0;
+    }
+}
+
+perfectNumbers()
+console.log("-".repeat(10))
+
+//Ödev 4
+function listPrimeNumber() {
+    let primes = []
+    for (let i = 1; i < 1000; i++)
+    {
+        let syc = 0;
+        for (let j = 1; j <= i; j++) if (i % j == 0) syc++;
+        if (syc == 2) primes.push(i);
+    }
+    console.log(`1000' e kadar olan asal sayılar : ${primes}`)
+}
+
+listPrimeNumber();
+
+
+
